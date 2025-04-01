301e86c91324ae685b4ae2805ddb503d2ba0e94e
Shreya Jayaraj <shreya291104@gmail.com>
2025-04-01 21:42:51 +0530

    created get-templatebyid

--- backend/controllers/latexController.js
+++ backend/controllers/latexController.js
@@ -8,7 +8,7 @@ exports.uploadImage = (req, res) => {
     if (req.file) {
       res.status(200).json({
         message: 'Image uploaded successfully!',
-        imagePath: `/uploads/images/${req.file.filename}`, 
+        imagePath: `/images/${req.file.filename}`, 
       });
     } else {
       res.status(400).json({ error: 'No image uploaded' });
@@ -19,6 +19,17 @@ exports.getTemplates = async (req, res) => {
   try {
     const templates = await Template.find();
     res.status(200).json(templates);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+exports.getTemplateById = async (req, res) => {
+  try {
+    const template = await Template.findById(req.params.id);
+    if (!template) {
+      return res.status(404).json({ message: "Template not found" });
+    }
+    res.status(200).json(template);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
